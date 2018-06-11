20fd70af26bb0da34ec7af6eafce927fed44e2e0
DimaGashko <dgashko@mail.ua>
2018-06-11 21:16:59 +0300

    createBaseFirstcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
    selector: 'app-root',
-   templateUrl: '',
+   templateUrl: './app/app.component.html',
+   styleUrls: ['./app/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+   appName: string = 'NgTodo';
+   appDescription: string = 'Todo List On Angular';
+}
