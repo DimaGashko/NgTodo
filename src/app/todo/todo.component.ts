5bc0773875f091c23bc243421d0b9f69f24d00ec
DimaGashko <dgashko@mail.ua>
2018-06-15 18:16:06 +0300

    edit

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from "@angular/core";
 @Component({
    selector: 'ng-todo__todo',
    templateUrl: './todo.component.html',
-   styleUrls: ['./todo.component.css']
+   styleUrls: ['./todo.component.css'],
+   host: {'class': 'todo'}
 })
 export class TodoComponent implements OnInit{
    ngOnInit() {
