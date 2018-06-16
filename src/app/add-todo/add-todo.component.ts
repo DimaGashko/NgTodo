11a073a3b9b6220b708d5b02cb8767c5e0de926f
DimaGashko <dgashko@mail.ua>
2018-06-16 13:29:33 +0300

    forms

--- src/app/add-todo/add-todo.component.ts
+++ src/app/add-todo/add-todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
    selector: 'ng-todo__add_todo',
@@ -6,42 +6,21 @@ import { Component, OnInit } from "@angular/core";
    styleUrls: ['./add-todo.component.css'],
 })
 export class AddTodoComponent implements OnInit {
-   private newTodoContent: string = '';
-   private maxContentLen: number = 50;
-   private minContentLen: number = 1;
+   newTodoContent: string = '';
 
-   add() {
-      const content = this.getFormatContent(this.newTodoContent);
+   add(content?: string): void {
+      if (content === void 0) content = this.newTodoContent;
+      content = this.getFormatContent(this.newTodoContent);
 
-      if (this.validateContent(content)) {
-         console.log(content + '|');
-      }
+      console.log(content);
+      this.newTodoContent = '';
    }
 
    getFormatContent(content: string): string {
       return content.trim();
    }
 
-   validateContent(content: string): boolean {
-      content = this.getFormatContent(content);
-
-      if (content.length === 0) {
-         this.errMes(`Min length: ${this.minContentLen}`);
-         return false;
-      }
-      if (content.length > this.maxContentLen) {
-         this.errMes(`Max length: ${this.maxContentLen}`);
-         return false;
-      }
-         
-      return true;
-   }
-
-   errMes(mes: string): void {
-      console.error(mes);
-   }
-
    ngOnInit(): void {
-      
+      console.log(this);
    }
 }
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,6 +8,6 @@ import { Component, OnInit } from "@angular/core";
 })
 export class TodoComponent implements OnInit {
    ngOnInit() {
-
+      
    }
 }
