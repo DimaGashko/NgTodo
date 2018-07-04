4e04a5390f8ade5aeb17315eaa3ff77168a913f3
DimaGashko <dgashko@mail.ua>
2018-07-04 11:43:34 +0300

    eidt

--- src/app/todo-db.ts
+++ src/app/todo-db.ts
@@ -1,14 +1,54 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+interface ITodo {
+   id: string;
+   content: string;
+   complete: boolean;
+}
+
+const PREFIX = 'ng-todo-todos'
+
+const DEFTodo: ITodo = {
+   id: '0',
+   content: 'Item',
+   complete: false,
+}
+
+const defaultTodos: ITodo[] = [
+   {id: '1', content: 'Bread', complete: true},
+   {id: '2', content: 'Water', complete: true},
+   {id: '3', content: 'Notebook', complete: false},
+   {id: '4', content: 'Paper', complete: false},
+];
+
 export class httpData implements InMemoryDbService {
    createDb() {
-      return [
-         {content: 'Bread', complete: true},
-         {content: 'Water', complete: true},
-         {content: 'Notebook', complete: false},
-         {content: 'Paper', complete: false},
-      ];
+      if (PREFIX in localStorage) {
+         const jsonTodos = localStorage[PREFIX];
+         return parseJsonTodos(jsonTodos);
+      }
+   
+      return defaultTodos;
    }
+}
+
+function parseJsonTodos(jsonTodos) {
+   try {
+      const todos: ITodo[] = JSON.parse(jsonTodos);
+
+      return getCorrectTodos(todos);
+
+   } catch (err) {
+      console.error(err);
+      return defaultTodos;
+   }
+
+}
+
+function getCorrectTodos(todos: ITodo[]): ITodo[] { 
+   return todos.map(todo => {
+      return Object.assign({}, DEFTodo, todo);
+   });
 }
 
 function getRandomId() {
